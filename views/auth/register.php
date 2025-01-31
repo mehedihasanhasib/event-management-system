18f042b20a6616905043b0cdc7a66e4828525d6c
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2025-01-31 09:44:11 +0600

    changed in config/db file

--- views/auth/register.php
+++ views/auth/register.php
@@ -11,33 +11,38 @@ ob_start()
                 </div>
                 <div class="card-body">
                     <form id="registration-from" action="<?= route('register.store') ?>" method="POST">
+
+                        <!-- Name -->
                         <div class="mb-3">
-                            <label for="fullName" class="form-label">Full Name</label>
+                            <label for="fullName" class="form-label">Full Name</label><span class="text-danger">*</span>
                             <input type="text" name="name" class="form-control" id="fullName" placeholder="Enter your full name" required />
-                            <!-- <span class="text-danger nameError validationError"></span> -->
                             <?php component('input-error', ['className' => ["nameError"]]) ?>
                         </div>
 
+                        <!-- Email -->
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email address</label>
+                            <label for="email" class="form-label">Email address</label><span class="text-danger">*</span>
                             <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email" required />
                             <?php component('input-error', ['className' => ["emailError"]]) ?>
                         </div>
 
+                        <!-- Password -->
                         <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
+                            <label for="password" class="form-label">Password</label><span class="text-danger">*</span>
                             <input type="password" name="password" class="form-control" id="password" placeholder="Create a password" required />
                             <?php component('input-error', ['className' => ["passwordError"]]) ?>
                         </div>
 
+                        <!-- Confirm Password -->
                         <div class="mb-3">
-                            <label for="confirm_password" class="form-label">Confirm Password</label>
+                            <label for="confirm_password" class="form-label">Confirm Password</label><span class="text-danger">*</span>
                             <input type="password" name="confirm_password" class="form-control" id="confirm_password" placeholder="Confirm password" required />
                             <?php component('input-error', ['className' => ["confirm_passwordError"]]) ?>
                         </div>
 
+                        <!-- Profile Picture -->
                         <div class="mb-3">
-                            <label for="profile_picture" class="form-label">Choose Profile Picture</label>
+                            <label for="profile_picture" class="form-label">Choose Profile Picture</label><span class="text-danger">*</span>
                             <input type="file" name="profile_picture" class="form-control" id="profile_picture" />
                             <?php component('input-error', ['className' => ["profile_pictureError"]]) ?>
                             <div class="mt-3">
@@ -65,7 +70,7 @@ ob_start()
         const registrationForm = $("#registration-from")
         const profileImageInput = $("#profile_picture")
 
-        profileImageInput.on("change", function(event){
+        profileImageInput.on("change", function(event) {
             preview(event, "profile-picture-preview");
         })
 
