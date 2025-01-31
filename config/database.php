6d6d2154a107b8e031eb4dc2ef1a5738a0a32c71
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2025-01-31 09:44:11 +0600

    changed in config/db file

--- config/database.php
+++ config/database.php
@@ -2,22 +2,10 @@
 
 return [
     'db' => [
-        // 'host' => 'mysql-10d95588-mehedihasanhasib98-0269.b.aivencloud.com',
-        // 'port' => 13849,
-        // 'dbname' => 'ollyo-test',
-        // 'username' => 'avnadmin',
-        // 'password' => 'AVNS_zZVrffogLuEb-tKu4CG',
-
         'host' => 'localhost',
         'port' => 3306,
         'dbname' => 'ollyo-test',
         'username' => 'root',
         'password' => 'root1234',
-
-        // 'host' => 'localhost',
-        // 'port' => 3306,
-        // 'dbname' => 'hasibinf_ollyotest',
-        // 'username' => 'hasibinf_hasib',
-        // 'password' => '8oK_e{2k2feW',
     ],
 ];
